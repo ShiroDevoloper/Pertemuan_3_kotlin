6c81ba2eda23b5281bdfad648763c641d644d663
Shirodevolop <emilioelva11@gmail.com>
2022-03-25 16:52:18 +0700

    Percobaan Data class copy() method

---
+++ src/main/kotlin/Data Kelas/DataClassC.kt
@@ -0,0 +1,13 @@
+package `Data Kelas`
+//Percobaan Data class copy() method
+//Untuk sebuah data kelas, kita dapat membuat sebuah objek salinan(copy) dengan beberapa property yang berbeda menggunakan fungsi copy().
+fun main(){
+    val meri = Student("Meri", 20)
+    val badu = meri.copy("Badu")
+    if(meri.equals(badu))
+        println("meri is equal to badu.")
+    else
+        println("meri is not equal to badu.")
+    println("Hashcode of meri: ${meri.hashCode()}")
+    println("Hashcode of badu: ${badu.hashCode()}")
+}
