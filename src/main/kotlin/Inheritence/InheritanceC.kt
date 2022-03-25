2329e627d261b9b4d412726034597f6ad7147565
Shirodevolop <emilioelva11@gmail.com>
2022-03-25 16:02:11 +0700

    Percobaan eksplisit akses modifier untuk meng-override fungsi pada kelas induk yaitu akses modifier open

---
+++ src/main/kotlin/Inheritence/InheritanceC.kt
@@ -0,0 +1,22 @@
+package Inheritence
+//Percobaan eksplisit akses modifier untuk meng-override fungsi pada kelas induk yaitu akses modifier open
+open class Teacher {
+    open fun teach() {
+        println("")
+    }
+    fun info(){
+        println("I'am a Teacher.")
+    }
+}
+class MathsTeacher : Teacher() {
+    override fun teach() {
+        println("Teaching Maths...")
+    }
+}
+fun main() {
+    val teacher = Teacher()
+    val mathsTeacher = MathsTeacher()
+    mathsTeacher.info()
+    teacher.teach()
+    mathsTeacher.teach()
+}
