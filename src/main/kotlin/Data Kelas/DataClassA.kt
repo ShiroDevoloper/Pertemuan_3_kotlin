6914a6ec80106732479254cccd5fb14fbfae6e12
Shirodevolop <emilioelva11@gmail.com>
2022-03-25 16:48:28 +0700

    Percobaan Data Class

---
+++ src/main/kotlin/Data Kelas/DataClassA.kt
@@ -0,0 +1,14 @@
+package `Data Kelas`
+//Percobaan Data Class
+//Kotlin memiliki solusi yang lebih baik untuk menangani data yaitu dengan menggunakan data kelas
+//Di Kotlin kita tidak perlu menulis atau men-generate semua data kelas seperti method Getter dan Setter di Java, Kotlin telah men-generate secara otomatis fungsi Getter dan Setter pada setiap anggota kelas
+
+data class Student(val name: String, val age: Int)
+fun main() {
+    val boni = Student("Boni", 21)
+    val meri = Student("Meri", 20)
+    println("Student Name is: ${boni.name}")
+    println("Student Age is: ${boni.age}")
+    println("Student Name is: ${meri.name}")
+    println("Student Age is: ${meri.age}")
+}
